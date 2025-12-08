ef2fc5997e282bd5f0bd07f89185bd0279fb8ead
Samuel Bohl <samuel.bohl@gmail.com>
2025-12-08 17:23:34 +0100

    split into manual and bot pages

---
+++ game-player/app/manual/page.tsx
@@ -0,0 +1,9 @@
+import GuessTheNumberGame from "@/components/guess-the-number-game";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <GuessTheNumberGame />
+    </main>
+  );
+}
